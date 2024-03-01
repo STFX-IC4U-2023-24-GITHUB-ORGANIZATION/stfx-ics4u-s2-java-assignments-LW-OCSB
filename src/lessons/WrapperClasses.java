3f71a04fca53374bf74a4cdd8d1f3339163e668d
e28166 <e28166@FXH-0015840124>
2024-03-01 10:32:12 -0500

    s

---
+++ src/lessons/WrapperClasses.java
@@ -0,0 +1,40 @@
+package lessons;
+import java.util.ArrayList;
+
+public class WrapperClasses {
+
+	public static void main(String[] args) {
+		
+		// You can't put primitives in ArrayLists
+		// Java provides classes which correspond to EVERY primitive type
+		// Wrapper classes encapsulate, or wrap primitives within an object
+		
+		// Wrapper classes include:
+		
+		/*
+			Byte - wraps byte
+			Short - short
+			Integer - int
+			Long - long
+			Float - float
+			Double - double
+			Character - char
+			Boolean - boolean
+		 */
+		
+		// ArrayList
+		ArrayList<Boolean> myArrayList = new ArrayList();
+		
+		// Wrapper classes 
+		boolean myBoolean1 = true;
+		Boolean myBoolean2 = true;
+
+		myArrayList.add(myBoolean1);  // converted boolean to Boolean "boxing"
+		myArrayList.add(myBoolean2);
+		
+		myArrayList.get(0).
+		
+		
+	}
+
+}
